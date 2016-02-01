348498e41269083b4cf045f82ffbd8e6ca22b00a
Joachim Lerman <joachimlerman@gmail.com>
2016-01-31 21:55:17 -0800

    can now upload and edi file to be parsed

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var mongodb = require('mongodb');
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
+var multer  =   require('multer');
 var url = 'mongodb://localhost:27017/test';
+var fs = require('fs');
 
 let edifact = require('../index.js');
 let validator = new edifact.Validator();
@@ -21,47 +23,80 @@ let elements;
 let components;
 let mydata = "{";
 
-
-var multer  =   require('multer');
-var app = express();
+var MongoClient = mongodb.MongoClient;
+//var url = 'mongodb://localhost:27017/mydb';
+var gfile;
 var storage = multer.diskStorage({
         destination: function (req, file, callback) {
             callback(null, './uploads');
         },
         filename: function (req, file, callback) {
-            callback(null, file.fieldname + '-' + Date.now());
+            //callback(null, file.fieldname + '-' + Date.now());
+            gfile = file.fieldname+'-'+Date.now();
+            callback(null, gfile);
+            //console.log("\n\nfile.fieldname = "+file.fieldname+ Date.now());
+
         }
     });
 var upload = multer({ storage : storage}).single('userPhoto');
 app.get('/',function(req,res){
     res.sendFile(__dirname + "/index.html");
-    });
-    app.post('/api/photo',function(req,res){
-        upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
+
+    MongoClient.connect(url, function(err, db) {
+        if(err){
+            console.log("unable to connect to the mongoDB server, Error:\n"+ err);
+        } else {
+            console.log('Connection established to, '+ url);
         }
-        res.end("File is uploaded");
+    });
+
+    
+
+    });
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+    if(err) {
+        return res.end("Error uploading file.");
+    }
+
+    //TODO do a bunch of shit
+    fs.readFile('uploads/'+gfile, 'utf8', function (err, data){
+        if (err) {
+                return console.log(err);
+            }
+        console.log(data);
+        documentedi = data; 
+        documentedi = documentedi.replace(/(?:\r\n|\r|\n)/g, '');
+        console.log("\n\ndocumentedi\n\n "+ documentedi);
+        parser.onopensegment = function (segment) {
+            elements = [];
+            result.push({ name: segment, elements: elements });
+        }
+        parser.onelement = function () {
+            components = [];
+            elements.push(components);
+        }
+        parser.oncomponent = function (value) {
+            components.push(value);
+        }
+        result = [];
+        parser.write(documentedi);
+        for (let i = 0; i < result.length; i++) {
+            mydata += '"' + result[i].name + '": [' + result[i].elements + "]";
+            console.log('"' + result[i].name + '": [' + result[i].elements + "]");
+        }
+        mydata += "}"
+        var html = mydata + '<br>' +
+                   '<a href="/">Try again.</a>';
+        console.log("documentedi \n"+ documentedi+ "\njson\n" +mydata);
+        res.send(html);
+        parser.close();
+        });
     });
 });
-
-
-
-
-
 
 
 app.use(bodyParser());
-
-app.get('/', function (req, res) {
-    var html = '<form action="/pictures/upload" method="POST" enctype="multipart/form-data">'+
-               'Select an image to upload:'+
-               '<input type="file" name="image">'+ 
-               ' <input type="submit" value="Upload Image">'+
-               '</form>';
-    res.send(html);
-});
-
 app.get('/toto.txt', function(req, res){
     res.send(req.body);
 })
@@ -72,6 +107,19 @@ app.listen(3000, function () {
 });
 
 app.post('/translate', function(req, res){
+
+
+    MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+    if(err) { return console.dir(err); }
+        db.collection('test', function(err, collection) {});
+        db.collection('test', {w:1}, function(err, collection) {});
+        db.createCollection('test', function(err, collection) {});
+        db.createCollection('test', {w:1}, function(err, collection) {});
+});
+
+
+
+
     documentedi = req.body.content;
     documentedi = documentedi.replace(/(?:\r\n|\r|\n)/g, '');
     console.log("\n\ndocumentedi\n\n "+ documentedi);
