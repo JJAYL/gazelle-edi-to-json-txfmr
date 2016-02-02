58d055815ecbac9547a8d96e707d82bdad1e6e93
Joachim Lerman <joachimlerman@gmail.com>
2016-02-02 00:01:55 -0800

    minor change

--- app/app.js
+++ app/app.js
@@ -104,10 +104,10 @@ app.post('/translate', function(req, res){
 
     MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
     if(err) { return console.dir(err); }
-        db.collection('test', function(err, collection) {});
-        db.collection('test', {w:1}, function(err, collection) {});
-        db.createCollection('test', function(err, collection) {});
-        db.createCollection('test', {w:1}, function(err, collection) {});
+       // db.collection('test', function(err, collection) {});
+       // db.collection('test', {w:1}, function(err, collection) {});
+       // db.createCollection('test', function(err, collection) {});
+       // db.createCollection('test', {w:1}, function(err, collection) {});
 });
 
 
